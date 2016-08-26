573b317d1d3bef565d566ed3c0b6f34dcc2d1818
Nicolás Ormeño <ni.ormeno@gmail.com>
2016-08-26 18:47:44 -0300

    Add HMVC

---
+++ application/modules/admin/controllers/Home.php
@@ -0,0 +1,15 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: normeno
+ * Date: 26-08-16
+ * Time: 18:42
+ */
+class Home extends MX_Controller
+{
+    public function index()
+    {
+        echo 'test';
+    }
+}
